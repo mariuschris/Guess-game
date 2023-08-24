35623704f611a5da2151f484b4b7db9042a4f7c4
Nwankwo Uchenna Marius <86554330+mariuschris@users.noreply.github.com>
2023-08-24 23:08:17 +0100

    Update index.js
    inserted loop

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ prompt.get("choice", function(err, result){
     
     let play = result.choice;
     console.log(play);
-
+    
+     
     //condition to play
     if (play == 1){
         //get username
@@ -20,12 +21,14 @@ prompt.get("choice", function(err, result){
             console.log(`Welcome ${result.username}!\nChoose a number from 1 to 10()\nGuess right, you WIN\nWrong, you loose`)
             console.log()
             //generate random number whole numbers from 1 to 10
-        let randomNumber = Math.floor(Math.random() * 10 ) + 1;
+            
+        let playAgain;   
+        do{
+            let randomNumber = Math.floor(Math.random() * 10 ) + 1;
         
-        //get user choice
-        prompt.get("input", function(err, result){
-           if (err) return;
-            let userInput = result.input
+            //get user choice
+            const prompt = require('prompt-sync')();
+            const userInput = prompt('Guess: ')
 
             //checks if the user guessed right
             if (userInput == randomNumber){
@@ -35,10 +38,22 @@ prompt.get("choice", function(err, result){
             else if (userInput > 10 || result.input < 1){
                 console.log('Please input a number between 1 and 10')
             }
-            else if (userInput !== randomNumber)
-            console.log('.......Ouch! Wrong Guess, You have Lost.........')
-        })
 
+            //check if the user guess wrong
+            else if (userInput !== randomNumber){
+            console.log('.......Ouch! Wrong Guess, You have Lost.........')
+
+            }
+            //Check if the user wants to play again of quit
+            const  chk = prompt('Enter 1 to play or any other key to exit: ')
+            if (chk == 1) {
+            playAgain = true;
+            } else playAgain = false;
+        }
+       //loop that restart the game
+        while (playAgain == true)
+        
+        console.log('................Goodbye................')
         })
         
          
